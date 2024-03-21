39ae175391dc74f411a2935a8536a7b79231642a
Naya252 <naya252naya@gmail.com>
2024-03-21 22:47:03 +0300

    feat: add callback to header

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,28 @@ import '@/styles/core.scss';
 import BaseComponent from '@/components/shared/base-component';
 import Header from './components/header/header-component';
 import Footer from './components/footer/footer-component';
+import { NAV_LINKS } from './shared/constants';
 
 export default class App {
   private readonly appContainer: BaseComponent;
   private readonly header: Header;
   private readonly main: BaseComponent;
   private readonly footer: Footer;
+  private readonly garage: BaseComponent;
+  private readonly winners: BaseComponent;
 
   constructor() {
     this.appContainer = new BaseComponent('div', ['app']);
 
-    this.header = new Header();
+    this.header = new Header((value: string) => {
+      this.navigate(value);
+    });
     this.main = new BaseComponent('div', ['content', 'container']);
+
+    this.garage = new BaseComponent('div', [], { id: 'garage' }, NAV_LINKS[0]);
+    this.winners = new BaseComponent('div', [], { id: 'winners' }, NAV_LINKS[1]);
+    this.main.append(this.garage);
+
     this.footer = new Footer();
 
     this.appContainer.append(this.header, this.main, this.footer);
@@ -22,5 +32,16 @@ export default class App {
   public init(): void {
     const { body } = document;
     this.appContainer.appendToParent(body);
+  }
+
+  private navigate(value: string): void {
+    const items = this.main.getChildren();
+    const item = items[0];
+
+    if (item !== null && item instanceof HTMLElement) {
+      if (value.toLocaleUpperCase() !== item.id) {
+        this.main.replaceChildren(value === NAV_LINKS[0] ? this.garage : this.winners);
+      }
+    }
   }
 }
--- src/components/header/header-component.ts
+++ src/components/header/header-component.ts
@@ -7,26 +7,32 @@ const createTitle = (): BaseComponent => {
   return title;
 };
 
-const createNav = (): BaseComponent => {
-  const nav = new BaseComponent('ul', ['navbar-nav', 'flex-row', 'flex-wrap', 'bd-navbar-nav']);
-  NAV_LINKS.forEach((el) => {
-    const item = new BaseComponent('li', ['nav-item']);
-    const link = new BaseComponent('a', ['nav-link'], { href: '#' }, el);
-    link.appendToParent(item);
-    item.appendToParent(nav);
-  });
-  return nav;
-};
-
 export default class Header extends BaseComponent {
-  constructor() {
+  private readonly onChangeLink: (value: string) => void;
+
+  constructor(onChangeLink: (value: string) => void) {
     super('header', ['header', 'navbar', 'navbar-dark', 'bd-navbar']);
+    this.onChangeLink = onChangeLink;
 
     const container = new BaseComponent('nav', ['container-xxl', 'navbar-dark', 'bd-navbar']);
     const title = createTitle();
-    const nav = createNav();
+    const nav = this.createNav();
 
     container.append(title, nav);
     this.append(container);
+  }
+
+  private createNav(): BaseComponent {
+    const nav = new BaseComponent('ul', ['navbar-nav', 'flex-row', 'flex-wrap', 'bd-navbar-nav']);
+    NAV_LINKS.forEach((el) => {
+      const item = new BaseComponent('li', ['nav-item']);
+      const link = new BaseComponent('a', ['nav-link'], { href: '#' }, el);
+      link.appendToParent(item);
+      link.addListener('click', () => {
+        this.onChangeLink(el);
+      });
+      item.appendToParent(nav);
+    });
+    return nav;
   }
 }
