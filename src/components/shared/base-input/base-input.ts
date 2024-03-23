a9977075034bc8b168511bfc35723aa457090b8a
Naya252 <naya252naya@gmail.com>
2024-03-23 20:34:59 +0300

    feat: add base input

---
+++ src/components/shared/base-input/base-input.ts
@@ -0,0 +1,69 @@
+import BaseComponent from '@/components/shared/base-component';
+import '@/components/shared/base-input/base-input.scss';
+
+type InputType =
+  | 'text'
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'time'
+  | 'url'
+  | 'week';
+
+export default class BaseInput extends BaseComponent {
+  private readonly input: BaseComponent;
+
+  constructor(
+    inputId: string,
+    labelText: string,
+    textId: string,
+    subtext: string,
+    attr: Record<string, string>,
+    inputType: InputType = 'text',
+  ) {
+    super('div', ['input-wrapper']);
+
+    const label = new BaseComponent('label', ['form-label'], { for: inputId }, labelText);
+    this.input = new BaseComponent('input', ['form-control'], {
+      ...attr,
+      id: inputId,
+      'aria-describedby': textId,
+      type: inputType,
+    });
+    const text = new BaseComponent('div', ['form-text'], { id: textId, text: subtext });
+
+    this.append(label, this.input, text);
+  }
+
+  public inputListener(eventName: string, listener: EventListenerOrEventListenerObject): void {
+    this.input.addListener(eventName, listener);
+  }
+
+  public getValue(): string {
+    const el = this.input.getElement();
+
+    if (!('value' in el)) {
+      throw new Error('not value');
+    }
+    if (typeof el.value !== 'string') {
+      throw new Error('not string');
+    }
+
+    return el.value;
+  }
+}
