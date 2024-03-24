d1db97a05ab64d06e83612c4a9e53b848ce47567
Naya252 <naya252naya@gmail.com>
2024-03-25 02:16:51 +0300

    feat: add methods for pagination

--- src/store/garage-store/store.ts
+++ src/store/garage-store/store.ts
@@ -2,9 +2,13 @@ import { type Car } from '@/types/types';
 
 export default class Garage {
   private cars: Car[];
+  private currentPage: number;
+  private allPages: number;
 
   constructor(carsData: Car[]) {
     this.cars = carsData;
+    this.currentPage = 1;
+    this.allPages = 1;
   }
 
   public getCars(): Car[] {
@@ -13,18 +17,22 @@ export default class Garage {
 
   public setCars(cars: Car[]): void {
     this.cars = cars;
+    this.changeAllPages();
   }
 
   public addCar(car: Car): void {
     this.cars.push(car);
+    this.changeAllPages();
   }
 
   public concatCars(cars: Car[]): void {
     this.cars = this.cars.concat(cars);
+    this.changeAllPages();
   }
 
   public removeCar(car: Car): void {
     this.cars = this.cars.filter((el) => el !== car);
+    this.changeAllPages();
   }
 
   public changeCar(car: Car): void {
@@ -33,5 +41,27 @@ export default class Garage {
       item.color = car.color;
       item.name = car.name;
     }
+  }
+
+  public getAllPages(): number {
+    return this.allPages;
+  }
+
+  public getCurrentPage(): number {
+    return this.currentPage;
+  }
+
+  public changeAllPages(): void {
+    this.allPages = Math.ceil(this.cars.length / 7);
+  }
+
+  public changeCurrentPage(value: number): void {
+    this.currentPage = value;
+  }
+
+  public getCarsForShow(): number[] {
+    const current = this.currentPage;
+    const end = current * 7;
+    return [end - 7, end - 1];
   }
 }
