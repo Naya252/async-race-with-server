e6fc25f84588b5cbaf7c80bb6ef3abd16297a9db
Naya252 <naya252naya@gmail.com>
2024-03-24 22:48:37 +0300

    feat: add service for generate random cars

---
+++ src/components/garage/services/garage-service.ts
@@ -0,0 +1,20 @@
+import { FIRST_PART_NAME, SECOND_PART_NAME } from '@/shared/constants';
+
+export function getRadomHex(): string {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  const hexR = r.toString(16).padStart(2, '0');
+  const hexG = g.toString(16).padStart(2, '0');
+  const hexB = b.toString(16).padStart(2, '0');
+
+  return `#${hexR}${hexG}${hexB}`;
+}
+
+export function getRandomName(): string {
+  const firstIndex = Math.floor(Math.random() * FIRST_PART_NAME.length);
+  const secondIndex = Math.floor(Math.random() * SECOND_PART_NAME.length);
+
+  return `${FIRST_PART_NAME[firstIndex]} ${SECOND_PART_NAME[secondIndex]}`;
+}
