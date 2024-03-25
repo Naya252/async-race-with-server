d41c6a4352df2c777d95b627fec638aa65736c60
Naya252 <naya252naya@gmail.com>
2024-03-25 23:00:55 +0300

    feat: add new components for car

--- src/components/garage/car-row/item.ts
+++ src/components/garage/car-row/item.ts
@@ -7,6 +7,9 @@ export default class Item extends BaseComponent {
   private readonly car: Car;
   private readonly editBtn: BaseButton;
   private readonly removeBtn: BaseButton;
+  private readonly startBtn: BaseButton;
+  private readonly returnBtn: BaseButton;
+  private readonly carName: BaseComponent;
   private readonly onOpenRemoveModal: (data: CarType) => void;
   private readonly onOpenChangeModal: (data: CarType) => void;
 
@@ -19,7 +22,12 @@ export default class Item extends BaseComponent {
     this.car = new Car(data);
     this.editBtn = new BaseButton('button', '', ['btn', 'btn-pz-primary', 'icon', 'edit']);
     this.removeBtn = new BaseButton('button', '', ['btn', 'btn-pz-primary', 'icon', 'remove']);
-    this.append(this.editBtn, this.removeBtn, this.car);
+    this.startBtn = new BaseButton('button', '', ['btn', 'btn-pz-primary', 'icon', 'start']);
+    this.returnBtn = new BaseButton('button', '', ['btn', 'btn-pz-primary', 'icon', 'return']);
+    this.carName = new BaseComponent('p', ['car-title'], {}, data.name);
+    const btnWrapper = new BaseComponent('div', ['car-btns']);
+    btnWrapper.append(this.removeBtn, this.editBtn, this.startBtn, this.returnBtn, this.carName);
+    this.append(btnWrapper, this.car);
 
     this.removeBtn.addListener('click', () => {
       this.onOpenRemoveModal(data);
@@ -35,5 +43,10 @@ export default class Item extends BaseComponent {
 
   public changeCar(data: CarType): void {
     this.car.setCar(data);
+    this.changeCarName(data.name);
+  }
+
+  private changeCarName(name: string): void {
+    this.carName.setTextContent(name);
   }
 }
