a759ab767e471911b7f29fdf12e676b7f720fa1b
Naya252 <naya252naya@gmail.com>
2024-03-29 12:00:55 +0300

    feat: add base table

---
+++ src/components/shared/base-table/base-table.ts
@@ -0,0 +1,96 @@
+import BaseComponent from '@/components/shared/base-component';
+import '@/components/shared/base-table/base-table.module.scss';
+import type { TableTitle } from '@/types/types';
+
+export default class BaseTable extends BaseComponent {
+  public readonly head: BaseComponent;
+  private readonly tr: BaseComponent;
+  private body: BaseComponent;
+  private readonly titles: TableTitle[];
+
+  constructor(titles: TableTitle[]) {
+    super('table', ['table', 'table-striped']);
+    this.titles = titles;
+
+    this.head = new BaseComponent('thead');
+    this.tr = new BaseComponent('tr');
+    this.head.append(this.tr);
+    this.body = new BaseComponent('tbody');
+
+    this.createHeaderItems();
+    this.append(this.head, this.body);
+  }
+
+  private createHeaderItems(): void {
+    this.titles.forEach((el) => {
+      const th = new BaseComponent('th', [...el.cssClasses, 'table-title'], { scope: 'col', id: el.key }, el.value);
+      this.tr.append(th);
+
+      if (el.isSort === true) {
+        th.addListener('click', () => {
+          const elements = this.tr.getChildren();
+          const element = th.getElement();
+          const id = element.getAttribute('id');
+
+          elements.forEach((elem) => {
+            if (elem instanceof HTMLElement) {
+              const elId = elem.getAttribute('id');
+              if (id !== elId) {
+                elem.classList.add('shadow-icon');
+                elem.classList.remove('active');
+                elem.classList.remove('desc');
+              }
+            }
+          });
+
+          if (element.classList.contains('shadow-icon')) {
+            element.classList.remove('shadow-icon');
+            element.classList.add('active');
+            return;
+          }
+          if (element.classList.contains('desc')) {
+            element.classList.remove('desc');
+            return;
+          }
+          if (element.classList.contains('active')) {
+            element.classList.add('desc');
+          }
+        });
+      }
+    });
+  }
+
+  public createBody(): void {
+    console.log(this.body);
+    this.body.remove();
+    this.body = new BaseComponent('tbody');
+    this.append(this.body);
+  }
+
+  public createBodyItem(item: Record<string, string | number | HTMLElement>): void {
+    const fields = Object.values(item);
+    const tr = new BaseComponent('tr');
+    const th = new BaseComponent('th', [], {}, String(fields[0]));
+    tr.append(th);
+
+    let i = 1;
+    while (i < this.titles.length) {
+      let td;
+      const data = fields[i];
+      if (typeof data === 'undefined') {
+        throw new Error('undefined');
+      }
+      if (data instanceof HTMLElement) {
+        td = new BaseComponent('td');
+        td.append(data);
+      } else {
+        td = new BaseComponent('td', [], {}, String(data));
+      }
+
+      tr.append(td);
+      i += 1;
+    }
+
+    this.body.append(tr);
+  }
+}
